a9b2bc7361b10b8d9305e1172736f95148bfc82e
radosz99 <241385@student.pwr.edu.pl>
2020-11-28 16:16:51 +0100

    Add finding minimum and maximum value in test results

--- calc.sh
+++ calc.sh
@@ -2,11 +2,14 @@
 
 rm averages.csv
 
-echo "Test;Average;Iterations;Generations average" >> averages.csv
+echo "Test;Average;Best;Worst;Iterations;Generations average" >> averages.csv
 for f in build/res*
 do
 	avg=`awk -F ";" '{ sum += $4 }; END { print sum/NR }' $f`
 	avg_iter=`awk -F ";" '{ sum += $5 }; END { print sum/NR }' $f`
+	best=`awk -F ";" '{ print $4 }' $f | sort -n | head -1`
+	worst=`awk -F ";" '{ print $4 }' $f | sort -n | tail -1`
 	amount=`awk -F ";" 'END { print NR}' $f`
-	echo "$f;$avg;$amount;$avg_iter" >> averages.csv
+	echo "$f;$avg;$best;$worst;$amount;$avg_iter" >> averages.csv
 done
+
